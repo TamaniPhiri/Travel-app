685fdcdf22c6cbc6c997cbcb9c19e97ba064002e
tamaniphiri <tamanigabriel0@gmail.com>
2024-09-06 21:38:17 +0200

    changes

--- lib/pages/onboard.dart
+++ lib/pages/onboard.dart
@@ -9,17 +9,41 @@ class OnBoardPage extends StatefulWidget {
 }
 
 class _OnBoardPageState extends State<OnBoardPage> {
+  int currentIndex = 0;
+  Widget dotIndicator(int index) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 400),
+      margin: EdgeInsets.only(right: 10),
+      width: index == currentIndex ? 40 : 10,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          PageView.builder(itemBuilder: (context, index) {
-            return Image.network(
-              onBoardData[index].image,
-              fit: BoxFit.cover,
-            );
-          })
+          PageView.builder(
+              itemCount: onBoardData.length,
+              itemBuilder: (context, index) {
+                return Image.network(
+                  onBoardData[index].image,
+                  fit: BoxFit.cover,
+                );
+              }),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 60),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    List.generate(onBoardData.length, (index) => null)
+                  ],
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
