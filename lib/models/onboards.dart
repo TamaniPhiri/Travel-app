300c1654358e5ce272b4ee7bcf8f7171a906e372
tamaniphiri <tamanigabriel0@gmail.com>
2024-09-06 21:15:39 +0200

    changes

--- lib/models/onboards.dart
+++ lib/models/onboards.dart
@@ -2,3 +2,11 @@ class OnBoards {
   final String image, text1, text2;
   OnBoards({required this.image, required this.text1, required this.text2});
 }
+
+List<OnBoards> onBoardData = [
+  OnBoards(
+      image:
+          "https://img.freepik.com/free-vector/website-designer-concept-illustration_114360-4449.jpg?size=626&ext=jpg&ga=GA1.1.795971097.1724933458&semt=ais_hybrid",
+      text1: "Enjoy your",
+      text2: "trip in nature")
+];
