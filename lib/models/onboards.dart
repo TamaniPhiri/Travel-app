4e820808700a9050885d2045077afb9712d9d440
tamaniphiri <tamanigabriel0@gmail.com>
2024-09-06 21:27:59 +0200

    changes

--- lib/models/onboards.dart
+++ lib/models/onboards.dart
@@ -11,12 +11,12 @@ List<OnBoards> onBoardData = [
       text2: "trip in nature"),
   OnBoards(
       image:
-          "https://img.freepik.com/free-vector/travel-people-character-with-suitcase-trip_107791-28091.jpg?ga=GA1.1.795971097.1724933458&semt=ais_hybrid",
-      text1: "Enjoy your",
-      text2: "trip in nature"),
+          "https://img.freepik.com/free-psd/girl-character-with-baggage-coffee-cup_23-2149838731.jpg?ga=GA1.1.795971097.1724933458&semt=ais_hybrid",
+      text1: "Start your",
+      text2: "journey in whatever"),
   OnBoards(
       image:
-          "https://img.freepik.com/free-vector/travel-people-character-with-suitcase-trip_107791-28091.jpg?ga=GA1.1.795971097.1724933458&semt=ais_hybrid",
-      text1: "Enjoy your",
-      text2: "trip in nature")
+          "https://img.freepik.com/free-vector/hand-drawn-flat-tourists-illustration_23-2149053947.jpg?ga=GA1.1.795971097.1724933458&semt=ais_hybrid",
+      text1: "Let\'s make",
+      text2: "your dream vacation")
 ];
